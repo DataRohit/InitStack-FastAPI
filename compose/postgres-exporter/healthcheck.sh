acab6da4c0d6e35ffffafdc8c1b7ef6601d1b50a
Rohit Vilas Ingole <rohit.vilas.ingole@gmail.com>
2025-12-15 22:25:24 +0530

    fix: exporter services healthcheck removed

--- compose/postgres-exporter/healthcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-wget -q --spider http://localhost:9187/metrics || exit 1
--- compose/redis-exporter/healthcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget --no-verbose --tries=1 --spider http://localhost:9121/metrics || exit 1
