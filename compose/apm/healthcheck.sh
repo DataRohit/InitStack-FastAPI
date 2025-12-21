74d1f6e3bf77abd9bd2fe0ebd2695efcb746d4fe
Rohit Vilas Ingole <rohit.vilas.ingole@gmail.com>
2025-12-21 08:03:14 +0530

    fix: formatting issues fixed

--- compose/apm/healthcheck.sh
+++ compose/apm/healthcheck.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-APM_HOST="${APM_SERVER_HOST:-0.0.0.0}"
-APM_PORT="${APM_SERVER_PORT:-8200}"
-
-if timeout 5 bash -c "</dev/tcp/${APM_HOST}/${APM_PORT}" 2>/dev/null; then
+if timeout 5 bash -c "</dev/tcp/${APM_SERVER_HOST}/${APM_SERVER_PORT}" 2>/dev/null; then
     echo "APM Server is healthy"
     exit 0
 else
